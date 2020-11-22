b3c62ed079e2275ce8580f65cbdf2f3cb8b8fdb2
Grace <73140860+Young-Grace@users.noreply.github.com>
2020-11-22 17:16:40 -0500

    Add files via upload

---
+++ SubscriberList/src/test/java/com/grace/sublist/SubscriberListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grace.sublist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
